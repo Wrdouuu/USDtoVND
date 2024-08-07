b5bfc01f3d2959e16137fc278fb51e463a97b231
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-07 13:19:41 +0700

    VND/USD

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace USDTOVND
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            float USD;
+            float VND;
+            Console.WriteLine("Amount of USD= ");
+            USD = float.Parse(Console.ReadLine());
+            Console.WriteLine("USD to VND= "+ USD * 23000 +"d");
+        }
+    }
+}
